076847fc7a61ceb61863b7145883d30533f7d6d3
mohamed adel <adel@orcas.io>
2018-06-03 16:46:06 +0200

    First commit

---
+++ private/db_credentials.php
@@ -0,0 +1,10 @@
+<?php
+// Keep database credentials in a separate file 
+// 1. Easy to exclude this file from source code managers
+// 2. Unique credentials on development and production servers
+// 3. unique credentials if working with multiple developers
+
+define("DB_SERVER" , "localhost");
+define("DB_USER" , "webuser");
+define("DB_PASS" , "123456");
+define("DB_NAME" , "chain_gang");
